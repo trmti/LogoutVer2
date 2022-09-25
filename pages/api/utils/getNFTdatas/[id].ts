02ce049be42e2d954d7d53c7d7a5f1fb982e0019
trmti <trmtikko@gmail.com>
2022-09-25 11:17:34 +0900

    create getNFTdatas page

---
+++ pages/api/utils/getNFTdatas/[id].ts
@@ -0,0 +1,32 @@
+import { nftContract } from '@/utils/contracts';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const id = req.query.id;
+  if (req.method === 'GET') {
+    if (typeof id === 'string') {
+      const results = await Promise.all([
+        (async () => {
+          return await nftContract.getLevel(id);
+        })(),
+        (async () => {
+          return await nftContract.getDamages(id);
+        })(),
+        (async () => {
+          return await nftContract.getMetaDataid(id);
+        })(),
+      ]);
+      if (results[0] && results[1].length && results[2]) {
+        res.status(200).json({ results });
+        return;
+      } else {
+        res.status(400).json({ message: 'failed' });
+        return;
+      }
+    }
+    res.status(400).json({ message: 'failed' });
+  }
+}
