94d4352618ba3c3ddad248aaa2eda9124c71a9e1
trmti <trmtikko@gmail.com>
2022-09-26 14:39:05 +0900

    create getWeeklySleeps endpoint

---
+++ pages/api/utils/getWeeklySleeps/[address].ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '@/utils/supabaseClient';
+import type { PostgrestResponse } from '@supabase/postgrest-js';
+
+type sleeptype = {
+  data: string;
+  duration: number;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const useraddress = req.query.address;
+  if (req.method === 'GET') {
+    const { data, error }: PostgrestResponse<{ sleeps: sleeptype[] }> =
+      await supabase.rpc('get_sleeps', { useraddress });
+
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+      return;
+    } else if (!data) {
+      res.status(400).json({ message: 'Data is not found' });
+      return;
+    } else {
+      res.status(200).json(data);
+    }
+  }
+}
