e5de7fe25079fa1fe58806fbd8729163f836f3b6
trmti <trmtikko@gmail.com>
2022-09-28 22:58:28 +0900

    create deleteTodayLog endpoint

---
+++ pages/api/auth/deleteTodayLog.ts
@@ -0,0 +1,27 @@
+import { supabase } from '@/utils/supabaseClient';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type params = {
+  address: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { address }: params = req.body;
+  try {
+    if (req.method === 'GET') {
+      const { error } = await supabase.rpc('remove_sleep', {
+        useraddress: address,
+      });
+      if (!error) {
+        res.status(200).json({ message: 'delete log success!' });
+      } else {
+        res.status(400).json({ message: 'delete log failed', error });
+      }
+    }
+  } catch (e) {
+    res.status(500).json({ message: 'Internal Server Error', e: e.message });
+  }
+}
